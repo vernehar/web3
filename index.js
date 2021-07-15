5271262b37748e99bf923af257b3e627b61bab13
vernehar <verne.hartikainen@gmail.com>
2021-07-15 19:17:44 +0300

    Simple ethereum balance checker website. Dependencies: web3

---
+++ index.js
@@ -0,0 +1,12 @@
+var provider = 'https://mainnet.infura.io/v3/4a200a3f339c403a8c2aa708873f5a5c';
+var web3Provider = new Web3.providers.HttpProvider(provider);
+var web3 = new Web3(web3Provider);
+
+window.onload = function() {
+    document.getElementById("button").addEventListener("click", function() {
+        address = document.getElementById("inputField").value
+        web3.eth.getBalance(address).then((result) => {
+        document.getElementById("data").innerHTML = "Your balance is "+result;
+        });
+    })
+}
