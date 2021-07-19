cf926efcc9aced01a778ecb3d6a1126cab20a47b
vernehar <verne.hartikainen@gmail.com>
2021-07-19 12:37:09 +0300

    Site now finds out when the specified address made it first uniswap (v2) trade. Eth address can be inputted or fetched from metamask

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var provider = 'https://mainnet.infura.io/v3/4a200a3f339c403a8c2aa708873f5a5c';
-var web3Provider = new Web3.providers.HttpProvider(provider);
-var web3 = new Web3(web3Provider);
+//var provider = 'https://mainnet.infura.io/v3/4a200a3f339c403a8c2aa708873f5a5c';
+//var web3Provider = new Web3.providers.HttpProvider(provider);
+//var web3 = new Web3(web3Provider);
+
+
+
+var web3 = new Web3(Web3.givenProvider || 'https://mainnet.infura.io/v3/4a200a3f339c403a8c2aa708873f5a5c');
+console.log(web3)
 
 window.onload = function() {
+
     document.getElementById("button").addEventListener("click", function() {
         address = document.getElementById("inputField").value
         web3.eth.getBalance(address).then((result) => {
--- utils/datafetch.js
+++ utils/datafetch.js
@@ -1,5 +1,4 @@
 let key
-let address
 let uniswapTransactions = []
 let uniswapRouterAddresses = ["0x7a250d5630b4cf539739df2c5dacb4c659f2488d", "0xf164fc0ec4e93095b804a4795bbe1e041497b92a"];
 
@@ -10,52 +9,74 @@ jQuery.when(
     key = json.key;
 });
 
-function getTransactions(){
+async function getTransactions(address) {
     let URL = "https://api.etherscan.io/api?module=account&action=txlist&address="+address+"&startblock=0&endblock=99999999&sort=asc&apikey="+key;
     // get transaction history of given address fron etherscan via api
+    uniswapTransactions = []
     let fetchTransactions = fetch(URL)
     fetchTransactions.then(response => {
         response.json().then(data => {
-        // wait for response and then get uniswap transactions from the data
-        for (i = 0; i < data.result.length; i++) {
-            if (uniswapRouterAddresses.includes(data.result[i].to.toLowerCase())) {
-                uniswapTransactions.push(data.result[i])
+        if (data.message = "OK"){
+            for (i = 0; i < data.result.length; i++) {
+                if (uniswapRouterAddresses.includes(data.result[i].to.toLowerCase())) {
+                    uniswapTransactions.push(data.result[i])
+                    }
+                }
+                // get timestamps of uniswap transactions from array and sort. Oldest is at index zero
+                timestamps = []
+            for (j = 0; j < uniswapTransactions.length; j++) {
+                timestamps.push(uniswapTransactions[j].timeStamp)
             }
+            timestamps = timestamps.sort()
+            uniswapTransactions.sort() //all that is needed is to sort the json. timestamps array not needed
+            formMessage(uniswapTransactions[0])
+        }else{
+            formMesssage(undefined)
         }
-        // get timestamps of uniswap transactions from array and sort. Oldest is at index zero
-        timestamps = []
-        for (j = 0; j < uniswapTransactions.length; j++) {
-            timestamps.push(uniswapTransactions[j].timeStamp)
-        }
-        timestamps = timestamps.sort()
-        uniswapTransactions.sort() //all that is needed is to sort the json. timestamps array not needed
-        console.log(uniswapTransactions[0])
-        return uniswapTransactions[0] //Return index zero from sorted JSON
         })
-    })
-}
 
-function formMessage(json) {
-    var myDate = "17-09-2020";
-    myDate = myDate.split("-");
-    var newDate = new Date( myDate[2], myDate[1] - 1, myDate[0]);
+        
+    })}
 
-    if (json.timeStamp < newDate.getTime()) {
-        message = "You made your first uniswap trade on "+Date(json.timeStamp)
+
+async function formMessage(data) {
+    if (data != undefined){
+        myDate = "17-09-2020"
+        myDate = myDate.split("-");
+        var newDate = new Date( myDate[2], myDate[1] - 1, myDate[0]);
+        timeStamp = new Date(data.timeStamp*1000)
+        message = "You made your first uniswap trade on "+timeStamp.toLocaleString()    
+        document.getElementById("data").innerHTML = message
     }else{
-        message = "You made your first uniswap trade on "+Date(json.timeStamp)
+        document.getElementById("data").innerHTML = "No uniswap trades or address not valid"
     }
+    
 
-    return message
 }
+
 
 window.onload = function() {
     document.getElementById("button").addEventListener("click", function() {
-        address = document.getElementById("inputField").value
-        messagejson = getTransactions()
-        console.log(messagejson)
-        if (messagejson){
-            document.getElementById("data").innerHTML = formMessage(messagejson)
-        }
-    })}
+        buttonClick()
 
+
+})
+
+    document.getElementById('enableEthereumButton').addEventListener('click', function(){
+        getAccount();
+});
+
+
+}
+
+function buttonClick() {
+    if (document.getElementById("inputField").value.length == 42){
+        getTransactions(document.getElementById("inputField").value)
+    }
+}
+
+async function getAccount() {
+    accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+    getTransactions(accounts[0])
+    document.getElementById('enableEthereumButton').innerHTML = "Connected " + accounts[0].slice(0,5) + "..."
+}
