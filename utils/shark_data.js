6c4293dc24c84f65e34c6a8ed114580667b99cd4
vernehar <verne.hartikainen@gmail.com>
2021-09-03 17:05:21 +0300

    Added a checker for free IDs in NFT contracts

--- utils/shark_data.js
+++ utils/shark_data.js
@@ -4,6 +4,8 @@ let sharkDAOAddress = "0xAe7f458667f1B30746354aBC3157907d9F6FD15E"
 let ticketBooth = "0xee2eBCcB7CDb34a8A822b589F9E8427C24351bfc"
 let fundingCycles = "0xf507B2A1dD7439201eb07F11E1d62AfB29216e2E"
 let terminalV1 = "0xd569D3CCE55b71a8a3f3C418c329A66e5f714431"
+let sushiSwapPoolAddress = "0x2240EF04Bb3e2601977D2d47Aa3287628aD71156"
+let USDCETHPoolAddress = "0x397FF1542f962076d0BFE58eA045FfA2d347ACa0"
 let nounSupply = 0;
 let nounsDaoTreasurySize = 0;
 let sharkTokenTotalSupply
@@ -13,6 +15,8 @@ let sharkDAOEthBalance
 let nounsDAOnounBalance
 let sharkDAOnounBalance
 let untappedBalance
+let ethUSDPrice
+let tokenPrice
 
 var app = new Vue({
     el: '#app',
@@ -28,7 +32,14 @@ var app = new Vue({
         tokensforeth:0,
         sharkdaonounvaluation:0,
         message: "",
-        message2: ""
+        message2: "",
+        sharktokenpriceUSD:0,
+        sharktokenpriceETH:0,
+        marketcap:0,
+        sharkdaoethusd:0,
+        marketcapminuseth:0,
+        nounusdvaluation:0
+
         }
         
     })
@@ -3092,11 +3103,714 @@ let terminalABI = [
     }
  ]
 
+let sushiLPTokenAbi = [
+    {
+       "inputs":[
+          
+       ],
+       "stateMutability":"nonpayable",
+       "type":"constructor"
+    },
+    {
+       "anonymous":false,
+       "inputs":[
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"owner",
+             "type":"address"
+          },
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"spender",
+             "type":"address"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"value",
+             "type":"uint256"
+          }
+       ],
+       "name":"Approval",
+       "type":"event"
+    },
+    {
+       "anonymous":false,
+       "inputs":[
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"sender",
+             "type":"address"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount0",
+             "type":"uint256"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount1",
+             "type":"uint256"
+          },
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          }
+       ],
+       "name":"Burn",
+       "type":"event"
+    },
+    {
+       "anonymous":false,
+       "inputs":[
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"sender",
+             "type":"address"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount0",
+             "type":"uint256"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount1",
+             "type":"uint256"
+          }
+       ],
+       "name":"Mint",
+       "type":"event"
+    },
+    {
+       "anonymous":false,
+       "inputs":[
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"sender",
+             "type":"address"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount0In",
+             "type":"uint256"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount1In",
+             "type":"uint256"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount0Out",
+             "type":"uint256"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"amount1Out",
+             "type":"uint256"
+          },
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          }
+       ],
+       "name":"Swap",
+       "type":"event"
+    },
+    {
+       "anonymous":false,
+       "inputs":[
+          {
+             "indexed":false,
+             "internalType":"uint112",
+             "name":"reserve0",
+             "type":"uint112"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint112",
+             "name":"reserve1",
+             "type":"uint112"
+          }
+       ],
+       "name":"Sync",
+       "type":"event"
+    },
+    {
+       "anonymous":false,
+       "inputs":[
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"from",
+             "type":"address"
+          },
+          {
+             "indexed":true,
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          },
+          {
+             "indexed":false,
+             "internalType":"uint256",
+             "name":"value",
+             "type":"uint256"
+          }
+       ],
+       "name":"Transfer",
+       "type":"event"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"DOMAIN_SEPARATOR",
+       "outputs":[
+          {
+             "internalType":"bytes32",
+             "name":"",
+             "type":"bytes32"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"MINIMUM_LIQUIDITY",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"PERMIT_TYPEHASH",
+       "outputs":[
+          {
+             "internalType":"bytes32",
+             "name":"",
+             "type":"bytes32"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"",
+             "type":"address"
+          },
+          {
+             "internalType":"address",
+             "name":"",
+             "type":"address"
+          }
+       ],
+       "name":"allowance",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"spender",
+             "type":"address"
+          },
+          {
+             "internalType":"uint256",
+             "name":"value",
+             "type":"uint256"
+          }
+       ],
+       "name":"approve",
+       "outputs":[
+          {
+             "internalType":"bool",
+             "name":"",
+             "type":"bool"
+          }
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"",
+             "type":"address"
+          }
+       ],
+       "name":"balanceOf",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          }
+       ],
+       "name":"burn",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"amount0",
+             "type":"uint256"
+          },
+          {
+             "internalType":"uint256",
+             "name":"amount1",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"decimals",
+       "outputs":[
+          {
+             "internalType":"uint8",
+             "name":"",
+             "type":"uint8"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"factory",
+       "outputs":[
+          {
+             "internalType":"address",
+             "name":"",
+             "type":"address"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"getReserves",
+       "outputs":[
+          {
+             "internalType":"uint112",
+             "name":"_reserve0",
+             "type":"uint112"
+          },
+          {
+             "internalType":"uint112",
+             "name":"_reserve1",
+             "type":"uint112"
+          },
+          {
+             "internalType":"uint32",
+             "name":"_blockTimestampLast",
+             "type":"uint32"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"_token0",
+             "type":"address"
+          },
+          {
+             "internalType":"address",
+             "name":"_token1",
+             "type":"address"
+          }
+       ],
+       "name":"initialize",
+       "outputs":[
+          
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"kLast",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          }
+       ],
+       "name":"mint",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"liquidity",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"name",
+       "outputs":[
+          {
+             "internalType":"string",
+             "name":"",
+             "type":"string"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"",
+             "type":"address"
+          }
+       ],
+       "name":"nonces",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"owner",
+             "type":"address"
+          },
+          {
+             "internalType":"address",
+             "name":"spender",
+             "type":"address"
+          },
+          {
+             "internalType":"uint256",
+             "name":"value",
+             "type":"uint256"
+          },
+          {
+             "internalType":"uint256",
+             "name":"deadline",
+             "type":"uint256"
+          },
+          {
+             "internalType":"uint8",
+             "name":"v",
+             "type":"uint8"
+          },
+          {
+             "internalType":"bytes32",
+             "name":"r",
+             "type":"bytes32"
+          },
+          {
+             "internalType":"bytes32",
+             "name":"s",
+             "type":"bytes32"
+          }
+       ],
+       "name":"permit",
+       "outputs":[
+          
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"price0CumulativeLast",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"price1CumulativeLast",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          }
+       ],
+       "name":"skim",
+       "outputs":[
+          
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"uint256",
+             "name":"amount0Out",
+             "type":"uint256"
+          },
+          {
+             "internalType":"uint256",
+             "name":"amount1Out",
+             "type":"uint256"
+          },
+          {
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          },
+          {
+             "internalType":"bytes",
+             "name":"data",
+             "type":"bytes"
+          }
+       ],
+       "name":"swap",
+       "outputs":[
+          
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"symbol",
+       "outputs":[
+          {
+             "internalType":"string",
+             "name":"",
+             "type":"string"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"sync",
+       "outputs":[
+          
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"token0",
+       "outputs":[
+          {
+             "internalType":"address",
+             "name":"",
+             "type":"address"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"token1",
+       "outputs":[
+          {
+             "internalType":"address",
+             "name":"",
+             "type":"address"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          
+       ],
+       "name":"totalSupply",
+       "outputs":[
+          {
+             "internalType":"uint256",
+             "name":"",
+             "type":"uint256"
+          }
+       ],
+       "stateMutability":"view",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          },
+          {
+             "internalType":"uint256",
+             "name":"value",
+             "type":"uint256"
+          }
+       ],
+       "name":"transfer",
+       "outputs":[
+          {
+             "internalType":"bool",
+             "name":"",
+             "type":"bool"
+          }
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    },
+    {
+       "inputs":[
+          {
+             "internalType":"address",
+             "name":"from",
+             "type":"address"
+          },
+          {
+             "internalType":"address",
+             "name":"to",
+             "type":"address"
+          },
+          {
+             "internalType":"uint256",
+             "name":"value",
+             "type":"uint256"
+          }
+       ],
+       "name":"transferFrom",
+       "outputs":[
+          {
+             "internalType":"bool",
+             "name":"",
+             "type":"bool"
+          }
+       ],
+       "stateMutability":"nonpayable",
+       "type":"function"
+    }
+ ]
+
 var web3 = new Web3(Web3.givenProvider || 'https://mainnet.infura.io/v3/4a200a3f339c403a8c2aa708873f5a5c');
 let contract = new web3.eth.Contract(minABI,nounAddress);
 let fundingCycleContract = new web3.eth.Contract(fundingCycleABI, fundingCycles);
 let ticketBoothContract = new web3.eth.Contract(ticketBoothABI, ticketBooth);
 let terminalContract = new web3.eth.Contract(terminalABI, terminalV1);
+let sushiSwapLP = new web3.eth.Contract(sushiLPTokenAbi, sushiSwapPoolAddress);
+let sushiUSDC = new web3.eth.Contract(sushiLPTokenAbi, USDCETHPoolAddress);
 
 //web3.eth.getBalance(sharkDAOAddress 
 
@@ -3156,6 +3870,16 @@ function fairPrice(supply, treasury){
 }
 }
 
+async function sharkTokenPrice(){
+    result = await sushiSwapLP.methods.getReserves().call();
+    return result
+}
+
+async function ethPrice(){
+    result = await sushiUSDC.methods.getReserves().call();
+    return result
+}
+
 getSharkTokenSupply().then(function (result) {
     sharkTokenTotalSupply = result
     app.sharktokensupply = result/1e18
@@ -3198,12 +3922,35 @@ getSharkDaoNounBalance().then(function(result){
         })
         })
         
+sharkTokenPrice().then(function(result){
+    tokenPrice = result._reserve1/result._reserve0
+    app.sharktokenpriceETH = tokenPrice
+ 
+})
 
+ethPrice().then(function(result){
+    app.sharktokenpriceUSD = tokenPrice * ((result._reserve0/result._reserve1)*1e12)
+    calculateValuation2()
+})
+
+function calculateValuation2(){
+    marketCap = tokenPrice * (sharkTokenTotalSupply)
+    app.marketcap = marketCap
+    ethHoldingValue = sharkDAOEthBalance * ethUSDPrice
+    app.sharkdaoethusd = ethHoldingValue
+    marketCapWithoutEth = marketCap - ethHoldingValue
+    app.marketcapminuseth = marketCapWithoutEth
+    app.nounsdaovaluation = marketCapWithoutEth/sharkDAOnounBalance
+
+}
 
 function calculateValuation(){
     percentageForOneEth = fundingCycleWeight/sharkTokenTotalSupply
+    console.log(percentageForOneEth)
     claimOfDAOEth = percentageForOneEth*sharkDAOEthBalance
+    console.log(claimOfDAOEth)
     valuation = (1-claimOfDAOEth)/percentageForOneEth;
+    console.log(valuation)
     valuationPerNoun = valuation/sharkDAOnounBalance
     return valuationPerNoun
 }
